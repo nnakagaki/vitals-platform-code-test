f73b323865f3bd59d7ad0f59d4467c947e7cd538
Norris Nakagaki <norrisnakagaki@gmail.com>
2014-12-09 10:50:33 -0500

    finished coding challenge

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,51 @@
 require 'award'
 
 def update_quality(awards)
+  blues = ['Blue First', 'Blue Compare']
+
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
+    next if award.name == 'Blue Distinction Plus'
+
+    if !blues.include?(award.name)
+      remove_quality(award)
+      blue_star_reduction(award)
+      if award.expires_in <= 0
+        remove_quality(award)
+        blue_star_reduction(award)
       end
     else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
+      add_quality(award)
+      if award.name == 'Blue First' && award.expires_in <= 0
+        add_quality(award)
+      elsif award.name == 'Blue Compare'
+        if award.expires_in <= 0
+          award.quality = 0
+        elsif award.expires_in < 6
+          2.times { add_quality(award) }
+        elsif award.expires_in < 11
+          add_quality(award)
         end
       end
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+
+    award.expires_in -= 1
+  end
+end
+
+def add_quality(award)
+  if award.quality < 50
+    award.quality += 1
+  end
+end
+
+def remove_quality(award)
+  if award.quality > 0
+    award.quality -= 1
+  end
+end
+
+def blue_star_reduction(award)
+  if award.name == 'Blue Star'
+    remove_quality(award)
   end
 end
